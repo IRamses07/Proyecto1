fc58c49c571f6de2bc73e4f2fc28af3b6c7909f9
German <gmonteror@ucenfotec.ac.cr>
2018-07-04 16:56:00 -0600

    commit

--- api/components/clientes/client.model.js
+++ api/components/clientes/client.model.js
@@ -16,6 +16,6 @@ let clientSchema = new mongoose.Schema({
     segundo_apellido:{type:String, required:true},
     telefono: {type:String, required:true},
     correo_electronico:{type:String, required:true},
-    ubicacion:{type: String,required:true}
+    ubicacion:{type: Array,required:true}
 });
 module.exports = mongoose.model('Cliente', clientSchema)
