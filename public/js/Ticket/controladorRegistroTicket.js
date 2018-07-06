767b820cbd03a2b07023adc0d6c8a00c01d8ed32
Kathe <36324710+KChaconC@users.noreply.github.com>
2018-07-06 17:56:04 -0600

    arreglo error

--- public/js/Ticket/controladorRegistroTicket.js
+++ public/js/Ticket/controladorRegistroTicket.js
@@ -3,7 +3,7 @@
 let botonRegistrarTicket = document.querySelector('#btnRegistrarTicket');
 botonRegistrarTicket.addEventListener('click', obtenerDatosTicket);
 
-listarSelectProyectos();
+
 
 let inptNombreCliente = document.querySelector('#txtnombreCliente');
 let inptUrgencia = document.querySelector('#sltUrgencia');
@@ -12,7 +12,7 @@ let inptPantallazoError = document.querySelector('#imgPantallazoError');
 let inptReferenciaTicket = document.querySelector('#sltTicket');
 
 let inptDescripcionError = document.querySelector('#txtdescripcion');
-
+listarSelectProyectos();
 
 
 function obtenerDatosTicket() {
@@ -103,10 +103,11 @@ function limpiarFormulario() {
 function listarSelectProyectos(){
     let sltproyectos = listarProyectos();
     let select =  document.querySelector('#sltProyecto');
+    select.options[0] = new Option("Seleccione un proyecto...", "");
 
     for(let i = 0; i < sltproyectos.length; i++){
-        select.options[i] = new Option(sltproyectos[i]['nombre_proyecto'], sltproyectos[i]['nombre_proyecto']);
-        select.value = sltproyectos[i];
+        select.options[i+1] = new Option(sltproyectos[i]['nombre_proyecto'], sltproyectos[i]['nombre_proyecto']);
+
     }
 }
 
