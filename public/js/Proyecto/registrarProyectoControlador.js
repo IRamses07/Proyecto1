97d611769e91cee8979c5292e27fa3499d84fef7
David Serrano Gutierrez <davidsegu9@gmail.com>
2018-07-06 11:39:37 -0600

    comit
    arreglar error con controladora
    agregar controlador para la lista aparte

--- public/js/Proyecto/registrarProyectoControlador.js
+++ public/js/Proyecto/registrarProyectoControlador.js
@@ -1,5 +1,5 @@
 "use strict";
-imprimirListaPersonas();
+
 
 //declaracion del boton y declaracion del event listener para dicho boton
 
@@ -69,26 +69,7 @@ function obtenerDatos() {
 
 }
 
-function imprimirListaPersonas(){
-    let infoProyecto = obtenerListaProyectos();
-    let tbody = document.querySelector('#tblProyectos tbody');
-    tbody.innerHTML = '';
 
-    for(let i = 0; i < infoProyecto.length; i++){
-        let fila = tbody.insertRow();
-
-        let cCedulaJuridica = fila.insertCell();
-        let cNombreProyecto = fila.insertCell();
-        let cNombreCliente = fila.insertCell();
-        let cFechaEntrega = fila.insertCell();
-
-        cCedulaJuridica.innerHTML = infoProyecto[i]['identificacion_juridica'];
-        cNombreProyecto.innerHTML = infoProyecto[i]['nombre_proyecto'];
-        cNombreCliente.innerHTML = infoProyecto[i]['nombre_cliente'];
-        cFechaEntrega.innerHTML = infoProyecto[i]['fecha_Entrega'];
-    }
-
-};
 
 function validarCampos() {
     let error = true;
