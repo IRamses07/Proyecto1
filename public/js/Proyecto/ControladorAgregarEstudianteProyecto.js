e641b173167d9aeb9777269aa0c8778b42e63a95
David Serrano Gutierrez <davidsegu9@gmail.com>
2018-07-14 13:43:22 -0600

    commit

--- public/js/Proyecto/ControladorAgregarEstudianteProyecto.js
+++ public/js/Proyecto/ControladorAgregarEstudianteProyecto.js
@@ -1,15 +1,17 @@
 llenarSelecProyectos();
 
+let selectProyecto = document.querySelector('#slProfeLider');
 
 function llenarSelecProyectos() {
-    let listaProfes = getProfessorData();
+    let infoPrfeIL = getCurrentUserData()['proyecto'];
+
+
+
     let select = document.querySelector('#slProfeLider');
     select.options[0] = new Option("Seleccione un profesor...", );
 
-    for (let i = 0; i < listaProyectosT.length; i++) {
-        select.options[i] = new Option(listaProyectosT[i]['nombre1'], listaProyectosT[i]['_id']);
-listaProfes[i]['id'];
-console.log(listaProfes[i]['id']);
+    for (let i = 0; i < listaProfes.length; i++) {
+        select.options[i] = new Option(listaProfes[i]['proyectos', 'nombre_proyecto'], listaProfes[i]['proyectos', 'id']);
 
     }
 }
