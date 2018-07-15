0b742b937d0b047aca8f16060621e972082ea365
Kathe <36324710+KChaconC@users.noreply.github.com>
2018-07-14 18:09:27 -0600

    cmmit
    claoudinary
    y cambios minimos

--- public/js/Ticket/servicioImagen.js
+++ public/js/Ticket/servicioImagen.js
@@ -2,7 +2,7 @@ let imgUrl = '';
 
 $(function() {
 
-    $.cloudinary.config({cloud_name: 'drbzc9dz2', api_key: '775983319571366' });
+    $.cloudinary.config({cloud_name: 'koffeedev', api_key: '697584759616556' });
 
         // Upload button
         let uploadButton = $('#file-upload');
@@ -10,13 +10,13 @@ $(function() {
         // Upload button event
         uploadButton.on('click', function(e){
             // Initiate upload
-            cloudinary.openUploadWidget({ cloud_name: 'drbzc9dz2', upload_preset: 'proyecto', tags: ['cgal']},
+            cloudinary.openUploadWidget({ cloud_name: 'koffeedev', upload_preset: 'proyecto', tags: ['cgal']},
             function(error, result) {
                 if(error) console.log(error);
                 // If NO error, log image data to console
                 let id = result[0].public_id;
                  console.log(id);
-                 imgUrl = 'https://res.cloudinary.com/drbzc9dz2/image/upload/' + id;
+                 imgUrl = 'https://res.cloudinary.com/koffeedev/image/upload/' + id;
                  imgUrl = processImage(id);
                 console.log(imgUrl);
                 document.querySelector('#imgErr').src = imgUrl;
