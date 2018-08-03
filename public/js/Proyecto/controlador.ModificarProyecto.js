d2f018ba2f41680ad8c4e40e41e3f453dc5b4169
david serrano gutierrez <davidsegu9@gmail.com>
2018-08-02 21:06:54 -0600

    llenar formulario
    sin terminar

--- public/js/Proyecto/controlador.ModificarProyecto.js
+++ public/js/Proyecto/controlador.ModificarProyecto.js
@@ -1,7 +1,17 @@
 'use strict';
 
 
+let inputNombreProyecto = document.querySelector('#txtNombreProyecto');
 
+let selectNombreCliente = document.querySelector('#slNombredelCliente');
+
+let inputIdentifiacionJuridica = document.querySelector('#txtIdentifiacionJuridica');
+
+let selectEstadoProyecto = document.querySelector('#sltEstado');
+
+let dateFechaEntrega = document.querySelector('#dtEntregaEstimada');
+
+let txtaDescripcion = document.querySelector('#txtDescripcion');
 
 
 function llenarFormulario() {
@@ -9,5 +19,9 @@ function llenarFormulario() {
     let id = localStorage.getItem('idP')
 
     let proyecto = obtenerProyectoId(id);
+
+    inputNombreProyecto.value = proyecto['nombre_proyecto'];
+    
+    
 
 }
