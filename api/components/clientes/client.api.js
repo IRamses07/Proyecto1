5d358535ff8151615ce59f055cb75e0a70b7f27d
German <gmonteror@ucenfotec.ac.cr>
2018-08-06 20:13:21 -0600

    commit
    esto se va a descontrollar

--- api/components/clientes/client.api.js
+++ api/components/clientes/client.api.js
@@ -24,7 +24,6 @@ module.exports.registrar = function (req, res) {
         passwordChange: req.body.passwordChange,
         foto: req.body.foto
     });
-
     nuevoCliente.save(function (error) {
         if (error) {
             res.json({
