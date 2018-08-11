7f2f19d09ad6f767876a78d47dc5c3c88f9f6b45
David Serrano Gutierrez <davidsegu9@gmail.com>
2018-08-10 18:30:09 -0600

    css y validacines

--- public/js/Estudiante/agregar_horas_controlador.js
+++ public/js/Estudiante/agregar_horas_controlador.js
@@ -3,11 +3,16 @@ llenarSelectEstudiantes();
 
 let botonRegistrar = document.querySelector('#btnHoras');
 botonRegistrar.addEventListener('click', optenerDatos);
+
+
 let selectNombreProyecto = document.querySelector('#nombreProyecto');
 
 let inputHoras = document.querySelector('#cantHoras');
 
 let dtfecha = document.querySelector('#dtFecha');
+
+let fromulario = document.querySelector('#frmHoras');
+
 
 function llenarSelectEstudiantes() {
 
@@ -26,18 +31,77 @@ function llenarSelectEstudiantes() {
 
 function optenerDatos() {
 
+    let error;
+
     let idE = getCurrentUserData()['_id'];
     let id = selectNombreProyecto.value;
     let horas = inputHoras.value;
     let fecha = dtfecha.value;
 
+    error = validar();
+
+    switch (error) {
+        case 1:
+
+
+            break;
+
+        case 2:
+
+            agregarHorasProyecto(idE, id, horas, fecha);
+            swal({
+                type: 'success',
+                title: 'Registro exitoso',
+                text: 'El Proyecto se registró adecuadamente',
+                confirmButtonText: 'Entendido'
+            });
+
+            fromulario.reset();
+
+            break;
+
+        default:
+            break;
+    }
+
     agregarHorasProyecto(idE, id, horas, fecha);
 }
 
 
-// id: { type: String },
-// tiempo: { type: String },
-// fecha: { type: String }
+
+function validar() {
+
+    let error;
+
+    if (selectNombreProyecto.value == '' || selectNombreProyecto.value == 'Seleccione un Proyecto') {
+        selectNombreProyecto.classList.add('error_input');
+        error = 1;
+    } else {
+        selectNombreProyecto.classList.remove('error_input');
+        error = 2;
+    }
+
+    if (inputHoras.value == '') {
+        inputHoras.classList.add('error_input');
+        error = 1;
+    } else {
+        inputHoras.classList.remove('error_input');
+        error = 2;
+    }
+
+    if (dtfecha.value == '') {
+        dtfecha.classList.add('error_input');
+        error = 1;
+    } else {
+        dtfecha.classList.remove('error_input');
+        error = 2;
+    }
+
+    return error;
+
+
+}
+
 
 
 
