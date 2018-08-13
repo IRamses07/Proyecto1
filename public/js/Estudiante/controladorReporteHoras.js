4699c5090cec4eb75574dde5be4c8c25b381e6df
German <gmonteror@ucenfotec.ac.cr>
2018-08-13 08:23:33 -0600

    commit

--- public/js/Estudiante/controladorReporteHoras.js
+++ public/js/Estudiante/controladorReporteHoras.js
@@ -26,7 +26,7 @@ function horasProyecto() {
         hTotales[0] = hTotales[0] + hTotales[1] / 60;
         info.push([pkeys[i], hTotales[0]]);
     }
-    drawChart(new google.visualization.BarChart(elm('#chart')), info, { title: 'Horas Proyecto' }, {
+    drawChart(new google.visualization.PieChart(elm('#chart')), info, { title: 'Horas Proyecto' }, {
         Proyecto: 'string',
         Horas: 'number',
     });
@@ -43,7 +43,7 @@ function horasTotales() {
     hTotales[0] += Math.trunc(hTotales[1] / 60);
     hTotales[1] = hTotales[1] - (Math.trunc(hTotales[1] / 60) * 60);
     hTotales[0] = hTotales[0] + hTotales[1] / 60;
-    drawChart(elm('#chart'), [
+    drawChart(new google.visualization.BarChart(elm('#chart')), [
         [estudiante[0].Nombre1, hTotales[0]]
     ], {
             bar: { groupWidth: '10%' }
@@ -51,4 +51,44 @@ function horasTotales() {
             nombre: 'string',
             Horas: 'number'
         });
+}
+function actividadProyecto() {
+    let estudiante = getInfoEstudiante(params.cedula);
+    let horas = estudiante[0].horas;
+    let proyectos = {};
+    for (let i = 0; i < estudiante[0].proyectos.length; i++) {
+        proyectos[estudiante[0].proyectos[i].nombre_proyecto] = [];
+        for (let j = 0; j < horas.length; j++) {
+            if (estudiante[0].proyectos[i]._id == horas[j].id) {
+                proyectos[estudiante[0].proyectos[i].nombre_proyecto].push(horas[j]);
+            }
+        }
+    }
+    let keys = Object.keys(proyectos);
+    let info = [];
+    elm('#chart').innerHTML = "";
+    for (let i = 0; i < keys.length; i++) {
+        for (let j = 0; j < proyectos[keys[i]].length; j++) {
+            let hTotales = [0, 0];
+            let tiempo = proyectos[keys[i]][j].tiempo.split(':');
+            hTotales[0] += Number(tiempo[0]);
+            hTotales[1] += Number(tiempo[1]);
+            hTotales[0] += Math.trunc(hTotales[1] / 60);
+            hTotales[1] = hTotales[1] - (Math.trunc(hTotales[1] / 60) * 60);
+            hTotales[0] = hTotales[0] + hTotales[1] / 60;
+            info.push([proyectos[keys[i]][j].fecha, hTotales[0]]);
+        }
+        let cont = createElm('div');
+        drawChart(new google.visualization.BarChart(cont),
+            info
+            , {
+                bar: { groupWidth: '90%' },
+                title:'Actividad para proyecto "'+keys[i]+'"'
+            }, {
+                Fecha: 'string',
+                Horas: 'number'
+            });
+        elm('#chart').appendChild(cont);
+    }
+
 }
