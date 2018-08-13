cf65eb568d2f0a8bdbf7dddcbd6d3b612d1b6c19
David Serrano Gutierrez <davidsegu9@gmail.com>
2018-08-12 19:24:42 -0600

    css de todo

--- public/js/Proyecto/controladorListaProyectosProfesor.js
+++ public/js/Proyecto/controladorListaProyectosProfesor.js
@@ -1,7 +1,7 @@
 'use strict'
 moveUser(true);
-let nomEstudianteTitulo = document.querySelector('#nomEstudiante');
-nomEstudianteTitulo.innerHTML = getCurrentUserData()['nombre1']+' '+getCurrentUserData()['apellido1'];
+// let nomEstudianteTitulo = document.querySelector('#nomEstudiante');
+// nomEstudianteTitulo.innerHTML = getCurrentUserData()['nombre1']+' '+getCurrentUserData()['apellido1'];
 let tbody = document.querySelector('#tblProyectosE tbody');
     tbody.innerHTML = '';
 
